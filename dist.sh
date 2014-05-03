050084f3db9c9ee8f83d70b5a93f87cd4bb32343
Bjarni R. Einarsson <bre@klaki.net>
2014-05-03 12:03:45 +0000

    Rename README to README.md (issue #3)

--- dist.sh
+++ dist.sh
@@ -6,15 +6,15 @@ mkdir -p tmp
 ln -s $(pwd) tmp/colormake-$VERSION
 (cd tmp && tar chvfz colormake-$VERSION.tar.gz colormake-$VERSION \
                      --exclude=tmp --exclude=.git --exclude=*.tar.gz)
-
-ln README tmp/README.txt
-
 mv tmp/*.tar.gz .
 rm -rf tmp
 
 echo
 echo 'Press ENTER to push to github and Klaki... [CTRL+C to abort]'
 read
+ln README.md README.txt
 scp colormake-$VERSION.tar.gz *.txt \
     bre@klaki.net:public_html/programs/colormake/
+rm -f README.txt
 git push
+git push --tags
